c992cca876214a28d8bf2979b9b1ac64f0c5ea6f
luish000 <luish000.lh@gmail.com>
2016-02-29 12:44:57 -0430

    added doc

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 'use strict'
 
-let path = require('path')
-let Quark = require('proton-quark')
-let _ = require('lodash')
+const path = require('path')
+const Quark = require('proton-quark')
+const _ = require('lodash')
+const controllersPath = path.join(this.proton.app.path, '/controllers')
 
-module.exports = class ControllersQuark extends Quark {
+
+/**
+ * @class ControllersQuark
+ * @classdesc This quark is for instance controllers
+ * @author Luis Hernandez
+ */
+class ControllersQuark extends Quark {
 
   constructor(proton) {
     super(proton)
   }
 
+  /**
+   * @override
+   * @method validate
+   * @description Ask if the proton.app.controllers object exist, if not exist
+   * the method create the proton.app.controllers object
+   * @author Luis Hernandez
+   */
   configure() {
-    if (!this.proton.app.controllers)
+    if (!this.proton.app.controllers) {
       this.proton.app.controllers = {}
-    return true
+    }
   }
 
+  /**
+   * @override
+   * @method initialize
+   * @description instance all controllers of the app
+   * @author Luis Hernandez
+   */
   initialize() {
-    let controllersPath = path.join(this.proton.app.path, '/controllers')
-    let controllers = require('require-all')(controllersPath)
-    _.forEach(controllers, Controller => new Controller(this.proton))
+    _.forEach(controllers, (Controller, fileName) => {
+      const controller = new Controller(this.proton)
+      controller.fileName = fileName
+      return controller
+    })
+  }
+
+  /**
+   * @method controllers
+   * @description This method get the export value of each controller present
+   * in the controllers folder
+   * @author Luis Hernandez
+   * @return {Array} - All controllers exported values as an array
+   */
+  get _controllers() {
+    return require('require-all')(controllersPath)
   }
 
 }
+
+module.exports = ControllersQuark
